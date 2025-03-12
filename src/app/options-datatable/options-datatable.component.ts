806a3f53ab24ba461e64de8efff88f8ccf1ca033
Yazmin Gutierrez Hernandez <230447@utxicotepec.edu.mx>
2025-03-12 13:33:09 -0600

    Tablas Dinamicas
    Se agrego otra nueva tabla con opciones personalizdas

---
+++ src/app/options-datatable/options-datatable.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Config } from 'datatables.net';
+import { DataTablesModule } from 'angular-datatables';
+@Component({
+  selector: 'app-options-datatable',
+  imports: [DataTablesModule],
+  templateUrl: './options-datatable.component.html',
+  styleUrl: './options-datatable.component.css'
+})
+export class OptionsDatatableComponent implements OnInit {
+  dtOptions: Config = {}
+  ngOnInit(): void {
+    this.dtOptions = {
+      ordering: true,
+      paging: true,
+      pageLength:30,
+      searching:false,
+      info:false,
+
+    }
+  }
+}
